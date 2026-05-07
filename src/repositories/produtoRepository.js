cb85e54024e0bb49e3da8680ffd121aa3e2c6e76
Lizzy-gt <luiz.gaspar@aluno.senai.br>
2026-05-07 11:11:27 -0300

    refatora ProdutoRepositorys para melhorar a legibilidade e corrigir a lógica de atualização

--- src/repositories/produtoRepository.js
+++ src/repositories/produtoRepository.js
@@ -1,33 +1,52 @@
-const pool = require('../config/database')
+const { off } = require("../app");
+const pool = require("../config/database");
 
 class ProdutoRepositorys {
-     async listarProdutos() { //vai retornar as informações do banco
-        const listarProdutos = await pool.query('SELECT * FROM produto')
-        return listarProdutos
-    }
+  async listarProdutos() {
+    //vai retornar as informações do banco
+    const listarProdutos = await pool.query("SELECT * FROM produto");
+    return listarProdutos;
+  }
 
-    async buscarProdutoPorId(id){
-        const mostrarProduto = await pool.query('SELECT * FROM produto WHERE id = ?', [id])
-        return mostrarProdut[0]
-    }
+  async buscarProdutoPorId(id) {
+    const mostrarProduto = await pool.query(
+      "SELECT * FROM produto WHERE id = ?",
+      [id],
+    );
+    return mostrarProdut[0];
+  }
 
-    async cadastrarProduto(dadosProdutos){
-        const resultadoCadastro = await pool.query('INSERT INTO produto SET ?', [dadosProdutos])
-        return resultadoCadastro.insertId
-    }
-    
-    async atualizarProduto(id, dadosDoProduto){
-        const produtoAtualizado = await pool.query('UPDATE produto SET ? WHERE id = ?', [dadosDoProduto, id])
-        return produtoAtualizado
-    }
+  async cadastrarProduto(dadosProdutos) {
+    const resultadoCadastro = await pool.query("INSERT INTO produto SET ?", [
+      dadosProdutos,
+    ]);
+    return resultadoCadastro.insertId;
+  }
 
-    async apagarProduto (id){
-        await pool.query('DELETE FROM produto WHERE id = ?', [id])
-        return true
+  async atualizarProduto(id, dadosDoProduto) {
+    const camposProduto = [];
+    const dadoProduto = []; //Serão responsaveis por amarzenar as informações de cada campo
+
+    for (const [key, dadoProduto] of Object.entries(dadosDoProduto)) {
+      //Vai caminhar por cada elemento do produto
+      camposProduto.push(`${key} =?`);
+      dadoProduto.push(values);
     }
+    if (camposProduto.length === 0) return null;
+
+    dadoProduto.push(id);
+
+    const query = `UPDATE produto SET ${camposProduto.join("," /*separar por virgula*/)} WHERE id = ?`;
+
+    const resultado = await pool.query(query, dadoProduto);
+  }
+
+  async apagarProduto(id) {
+    await pool.query("DELETE FROM produto WHERE id = ?", [id]);
+    return true;
+  }
 }
 
-module.exports = ProdutoRepositorys()
+module.exports = ProdutoRepositorys();
 
 //Aqui não vai validar os dados
-
